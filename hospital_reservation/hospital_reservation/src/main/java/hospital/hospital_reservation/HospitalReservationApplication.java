1ad0a6bcca438d4023796fdd4d5c55bf266b6510
jmd5314 <jmd5314@gmail.com>
2023-07-18 13:52:34 +0900

    Initial commit

---
+++ hospital_reservation/hospital_reservation/src/main/java/hospital/hospital_reservation/HospitalReservationApplication.java
@@ -0,0 +1,13 @@
+package hospital.hospital_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalReservationApplication.class, args);
+	}
+
+}
---
+++ hospital_reservation/hospital_reservation/src/test/java/hospital/hospital_reservation/HospitalReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package hospital.hospital_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
